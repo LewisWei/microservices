e3ab4da21db254623c909eb31f751105de36543f
Daniel Wei <youthdap@163.com>
2017-02-27 22:19:36 +0800

    之前使用的1.5.1版本及对应的 D 版本 Cloud 有 Bug，因此将 boot 版本降为 1.4.4 ，将 cloud 降至 Camden.SR5。

--- reservation-service/src/main/java/com/lewis/cloud/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/lewis/cloud/ReservationServiceApplication.java
@@ -1,13 +1,17 @@
 package com.lewis.cloud;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -29,6 +33,19 @@ public class ReservationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReservationServiceApplication.class, args);
+    }
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${message}")
+    private String message;
+
+    @RequestMapping("/message")
+    String message() {
+        return this.message;
     }
 }
 
